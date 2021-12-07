6919e4e38bc87e6342a98ebfd0725eec99195224
jhs60110 <58292007+jhs60110@users.noreply.github.com>
2021-12-07 16:25:12 +0900

    Add files via upload
    hadoop WordCount를 위한 mapreduce 파일

---
+++ WordCountReducer.java
@@ -0,0 +1,27 @@
+package WordCount;
+ 
+import java.io.IOException;
+ 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+ 
+public class WordCountReducer extends 
+Reducer<Text, IntWritable, Text, IntWritable> {
+ 
+    private IntWritable res = new IntWritable();
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values,
+            Context context)
+            throws IOException, InterruptedException {
+        // TODO Auto-generated method stub
+        int sum = 0;
+        for(IntWritable val : values)
+        {
+            sum += val.get();
+            //sum++;
+        }
+        res.set(sum);
+        context.write(key, res);
+    }
+}
